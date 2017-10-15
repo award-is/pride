01c80ac72d1ac92b09c0e727f8835aa33235dcb8
Paul Casper <kasper_ps_14@mf.grsu.by>
2017-10-15 14:29:38 +0300

    Add SpringBoot starter (#1)

---
+++ src/main/java/org/awardis/pride/PrideApplication.java
@@ -0,0 +1,12 @@
+package org.awardis.pride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrideApplication.class, args);
+	}
+}
---
+++ src/test/java/org/awardis/pride/PrideApplicationTests.java
@@ -0,0 +1,16 @@
+package org.awardis.pride;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrideApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
